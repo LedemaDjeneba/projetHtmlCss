ffadf9d8799e9a366f34eda1f71f0fad6e594591
Djeneba Aubriet <michaeletdjeneba@hotmail.fr>
2023-01-09 21:53:35 +0100

    v1

---
+++ contentParametresRecommencer.js
@@ -0,0 +1,5 @@
+let iconeMenu = document.querySelector('.iconeMenu')
+iconeMenu.addEventListener('click', () => { location.href = "ouwnList" })
+
+let button = document.querySelector('button')
+button.addEventListener('click')
